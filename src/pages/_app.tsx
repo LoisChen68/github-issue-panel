6332cb90ced519998fd4ada0112846c00368594e
Lois Chen <a8283626@gmail.com>
2023-03-19 22:25:01 +0800

    feat: add Layout style, panel page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '@/styles/globals.scss'
+import style from '@/styles/Layout.module.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <div className={style.wrapper}>
+      <div className={style['nav-bar-wrapper']}>
+        <div className={style['nav-bar-container']}>
+          <span>Navbar</span>
+        </div>
+      </div>
+      <div className={style.container}>
+        <Component {...pageProps} />
+      </div>
+    </div>
   )
 }
