86db691b79a91ed1fa776ad564352f3853197012
Lois Chen <a8283626@gmail.com>
2023-04-05 19:11:07 +0800

    feat: modify HomePage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import style from '@/styles/Layout.module.scss'
 import type { AppProps } from 'next/app'
 import router from 'next/router'
 import ModalContextProvider from '@/contexts/ModalContext'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -16,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ModalContextProvider>
       <div className={style['nav-bar-wrapper']}>
         <div className={style['nav-bar-container']}>
-          <span>Navbar</span>
+          <Link href='/panel'>Issues</Link>
           <button onClick={handleLogoutClick}>Log Out</button>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
+import { useEffect } from 'react'
+import style from '../styles/Home.module.scss'
+
 function HomePage() {
   const handleLoginClick = () => {
     window.location.href = `https://github.com/login/oauth/authorize?scope=public_repo&client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
   }
 
+
   return (
-    <>
+    <div className={style.wrapper}>
+      <h1 className={style.h1}>GitHub Issue Panel</h1>
       <button onClick={handleLoginClick}>
         登入 GitHub
       </button>
-    </>
+    </div>
   )
 }
 
